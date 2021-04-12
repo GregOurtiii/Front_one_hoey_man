8b62233a4f234bf5eeae3ee0cf71b62acf937581
clercgreg.ggc@gmail.com <clercgreg.ggc@gmail.com>
2021-04-12 17:26:54 +0200

    Ajout des parametres de routes, de la page register et de la routes entre login et register

--- angular-one-hoey-man/src/app/app.module.ts
+++ angular-one-hoey-man/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes : Routes = [ {path: "", component: LoginComponent}, 
+                             {path: "register", component: RegisterComponent} ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
